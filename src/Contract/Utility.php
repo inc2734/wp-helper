fd34ce16b200d12bb551c98745aea4f49cbd0a83
inc2734 <inc@2inc.org>
2020-10-02 14:03:51 +0900

    Add $args to get_custom_post_types, get_taxonomies

--- src/Contract/Utility.php
+++ src/Contract/Utility.php
@@ -13,47 +13,39 @@ trait Utility {
 	 * Return custom post type names.
 	 * This method only works correctly after the init hook.
 	 *
+	 * @param array $args An array of key => value arguments to match against the post type objects.
 	 * @return array
 	 */
-	public static function get_custom_post_types() {
-		$post_types = wp_cache_get( 'inc2734-wp-helper-custom-post-types' );
-		if ( is_array( $post_types ) ) {
-			return $post_types;
-		}
-
-		$post_types = get_post_types(
+	public static function get_custom_post_types( $args = [] ) {
+		$args = array_merge(
+			$args,
 			[
 				'public'   => true,
 				'_builtin' => false,
 			]
 		);
 
-		$post_types = $post_types ? $post_types : [];
+		$post_types = get_post_types( $args );
 
-		wp_cache_set( 'inc2734-wp-helper-custom-post-types', $post_types );
-		return $post_types;
+		return $post_types ? $post_types : [];
 	}
 
 	/**
 	 * Return custom taxonomy names.
 	 * This method only works correctly after the init hook.
 	 *
+	 * @param array $args An array of key => value arguments to match against the taxonomy objects.
 	 * @return array
 	 */
-	public static function get_taxonomies() {
-		$taxonomies = wp_cache_get( 'inc2734-wp-helper-taxonomies' );
-		if ( is_array( $taxonomies ) ) {
-			return $taxonomies;
-		}
-
-		$taxonomies = get_taxonomies(
+	public static function get_taxonomies( $args = [] ) {
+		$args = array_merge(
+			$args,
 			[
 				'public'   => true,
 				'_builtin' => false,
 			]
 		);
 
-		wp_cache_set( 'inc2734-wp-helper-taxonomies', $taxonomies );
-		return $taxonomies;
+		return get_taxonomies( $args );
 	}
 }
